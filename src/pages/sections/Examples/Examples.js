bf95400bd8df9b8e886474f6c32f82b5fa54b640
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-01-16 12:10:17 +0300

    Change audio and video to embed code

--- src/pages/sections/Examples/Examples.js
+++ src/pages/sections/Examples/Examples.js
@@ -1,20 +1,36 @@
 import React from "react"
 import "./Examples.scss"
-import AudioControl from "../../../components/AudioControl/AudioControl"
-import VideoControl from "../../../components/VideoControl/VideoControl"
-import AudioOne from "./rostok1.mp3"
-import AudioTwo from "./rostok2.mp3"
-import VideoOne from "./angel8.mp4"
-import VideoOneWebM from "./angel8.webm"
 
 function Examples() {
   return (
     <section className="examples" id="examples">
       <h2 style={{ textAlign: "center" }}>Примеры работ</h2>
-      <AudioControl srcMP3={AudioOne} id="audio-one" />
+      <iframe
+        width="100%"
+        height="23%"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/743937409&color=%2300aabb&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
       <br />
-      <AudioControl srcMP3={AudioTwo} id="audio-two" />
-      <VideoControl srcMP4={VideoOne} srcWEBM={VideoOneWebM} id="video-one" />
+      <iframe
+        width="100%"
+        height="23%"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/743938027&color=%2300aabb&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
+      <br />
+      <iframe
+        width="100%"
+        height="53%%"
+        src="https://www.youtube.com/embed/zVxlVbssCMk"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </section>
   )
 }
